d6f059ff9ef0c1e350c5194db73160cb56059d1c
Elijah Parker <elijahparker@gmail.com>
2016-11-03 16:57:33 -0500

    testing ble

--- main.js
+++ main.js
@@ -1355,7 +1355,7 @@ function startScan() {
         setTimeout(function() {
             if (noble.state == "poweredOn") {
                 console.log("Starting BLE scan...");
-                noble.startScanning();//['b8e0606762ad41ba9231206ae80ab550']);
+                noble.startScanning(['b8e0606762ad41ba9231206ae80ab550']);
             }
         }, 5000);
     }
