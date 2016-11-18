74c1b215319c05edfa7ec7311b09edb13dd4b4d2
Elijah Parker <elijahparker@gmail.com>
2016-11-18 09:42:19 -0600

    typo bugfix that broke XMP write to card

--- intervalometer/intervalometer.js
+++ intervalometer/intervalometer.js
@@ -583,7 +583,7 @@ intervalometer.writeXMPs = function(clipNumber, destinationFolder, callback) {
                 }
                 var xmpFile = destinationFolder + "/" + data[i].fileNumberString + ".xmp";
                 console.log("Writing " + xmpFile);
-                var desc = name + " created with the Timelapse+ VIEW\nImage #" + data[i].fileNumberString + "\nBase Exposure: " + ata[i].evCorrection - smoothCorrection;
+                var desc = name + " created with the Timelapse+ VIEW\nImage #" + data[i].fileNumberString + "\nBase Exposure: " + data[i].evCorrection - smoothCorrection;
                 image.writeXMP(xmpFile, data[i].evCorrection - smoothCorrection, desc, name);
             }
             if (callback) callback();
