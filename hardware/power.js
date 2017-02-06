bb2c5f8c095bc7aae1ed816b8a26e6424d608183
Elijah Parker <elijahparker@gmail.com>
2017-02-05 22:34:01 -0600

    battery testing

--- hardware/power.js
+++ hardware/power.js
@@ -204,7 +204,9 @@ function getPowerStats(callback) {
                 if(stats.batteryWarning) stats.batteryPercent = 0;
                 //console.log(stats);
                 var logString = stats.batteryPercent + "," + stats.batteryVoltage + "," + stats.batteryDischargeCurrent + ',' + stats.batteryWarning + ',' + stats.shutdownNow;
-                exec('echo "' + logString + '" >> /home/root/powerlog.txt', function(){});
+                exec('echo "' + logString + '" >> /home/root/powerlog.txt', function(err, stderr){
+                    if(err) echo stderr;
+                });
 
                 callback && callback(null, stats);
             } else {
