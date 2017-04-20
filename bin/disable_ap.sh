36c14393e9541250961f8b1562bc055436772e80
Elijah Parker <elijahparker@gmail.com>
2017-04-20 14:19:52 -0500

    using virtual wlan1 for local ap

--- bin/disable_ap.sh
+++ bin/disable_ap.sh
@@ -3,6 +3,6 @@ killall hostapd
 killall wpa_supplicant
 killall dhclient
 rm /run/hostapd.pid
-rm /var/run/hostapd/wlan0
-ip addr flush dev wlan0
-ifconfig wlan0 down
+rm /var/run/hostapd/wlan1
+ip addr flush dev wlan1
+ifconfig wlan1 down
--- bin/enable_ap.sh
+++ bin/enable_ap.sh
@@ -3,8 +3,8 @@ killall wpa_supplicant
 killall hostapd
 killall dhclient
 rm /run/hostapd.pid
-rm /var/run/hostapd/wlan0
-ip addr flush dev wlan0
+rm /var/run/hostapd/wlan1
+ip addr flush dev wlan1
 /usr/sbin/hostapd -B -P /run/hostapd.pid /etc/hostapd/hostapd.conf
-ip addr add 10.0.0.1/24 dev wlan0
-ip route add default via 10.0.0.1
+ip addr add 10.0.0.1/24 dev wlan1
+#ip route add default via 10.0.0.1
