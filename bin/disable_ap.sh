81d1a3e9bd76ab0ec9cdfeafca921d6958cbf302
Elijah Parker <elijahparker@gmail.com>
2017-05-03 16:31:48 -0500

    reverted wifi to single mode

--- bin/disable_ap.sh
+++ bin/disable_ap.sh
@@ -1,8 +1,17 @@
 #!/bin.sh
+
+## dual method (unstable)
+#killall hostapd
+#rm /run/hostapd.pid
+#rm /var/run/hostapd/wlan1
+#ip addr flush dev wlan1
+#ifconfig wlan1 down
+
+## single device method
 killall hostapd
-#killall wpa_supplicant
-#killall dhclient
+killall wpa_supplicant
+killall dhclient
 rm /run/hostapd.pid
-rm /var/run/hostapd/wlan1
-ip addr flush dev wlan1
-ifconfig wlan1 down
+rm /var/run/hostapd/wlan0
+ip addr flush dev wlan0
+ifconfig wlan0 down
--- bin/enable_ap.sh
+++ bin/enable_ap.sh
@@ -1,10 +1,18 @@
 #!/bin.sh
-#killall wpa_supplicant
+
+## dual method (unstable)
+#killall hostapd
+#rm /run/hostapd.pid
+#rm /var/run/hostapd/wlan1
+#ip addr flush dev wlan1
+#/usr/local/bin/hostapd -B -P /run/hostapd.pid /etc/hostapd/hostapd.conf
+#ip addr add 10.0.0.1/24 dev wlan1
+
+## single device method
 killall hostapd
-#killall dhclient
 rm /run/hostapd.pid
-rm /var/run/hostapd/wlan1
-ip addr flush dev wlan1
+rm /var/run/hostapd/wlan0
+ip addr flush dev wlan0
 /usr/local/bin/hostapd -B -P /run/hostapd.pid /etc/hostapd/hostapd.conf
-ip addr add 10.0.0.1/24 dev wlan1
+ip addr add 10.0.0.1/24 dev wlan0
 #ip route add default via 10.0.0.1
