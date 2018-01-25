42763791f403224a47deab2c151b43ab1a1f4399
Elijah Parker <elijahparker@gmail.com>
2018-01-25 14:32:24 -0600

    add CORS

--- system/app.js
+++ system/app.js
@@ -25,7 +25,8 @@ express.use(Express.static('/home/view/current/frontend/www'));
 
 express.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, access-control-allow-origin");
   next();
 });
 
