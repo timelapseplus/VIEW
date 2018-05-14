e9738b6dce2f52534e5b43cd3c91fd71ee50aa4e
Elijah Parker <elijahparker@gmail.com>
2018-05-14 12:14:04 -0500

    olympus lv

--- lib/libgphoto2_ptp2_updates/ptp.h
+++ lib/libgphoto2_ptp2_updates/ptp.h
@@ -2150,7 +2150,7 @@ typedef struct _PTPCanonEOSDeviceInfo {
 #define PTP_DPC_OLYMPUS_ISOBracket			0xD15D
 #define PTP_DPC_OLYMPUS_MyModeStatus			0xD15E
 
-#define PTP_DPC_OLYMPUS_LiveViewMode			0xD06D
+#define PTP_DPC_OLYMPUS_LiveViewModeOM			0xD06D
 
 /* Sony A900 */
 #define PTP_DPC_SONY_DPCCompensation			0xD200
