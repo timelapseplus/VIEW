6acd60bb8754c27d8cf7bda76bb59ece170457b5
Elijah Parker <elijahparker@gmail.com>
2018-07-14 14:07:11 -0500

    bugfixes

--- intervalometer/exposure.js
+++ intervalometer/exposure.js
@@ -268,11 +268,11 @@ function calculateDelta(currentEv, lastPhotoLum, config) {
         exp.status.nightRefEv = lastPhotoLum * nightRatio + -1.5 * (1 - nightRatio);
         exp.status.dayRefEv = lastPhotoLum * (1 - nightRatio);
         exp.status.fixedRefEv = lastPhotoLum;
-        exp.status.manualOffsetEv = getEvOffsetScale(currentEv, lastPhotoLum) - lastPhotoLum;
+        exp.status.manualOffsetEv = lastPhotoLum - getEvOffsetScale(currentEv, lastPhotoLum);
         console.log("EXPOSURE: lastPhotoLum =", lastPhotoLum);
         console.log("EXPOSURE: exp.status.manualOffsetEv =", exp.status.manualOffsetEv);
         console.log("EXPOSURE: getEvOffsetScale(currentEv, lastPhotoLum) =", getEvOffsetScale(currentEv, lastPhotoLum));
-        exp.status.offsetEv = getEvOffsetScale(currentEv, lastPhotoLum) - exp.status.manualOffsetEv;
+        exp.status.offsetEv = getEvOffsetScale(currentEv, lastPhotoLum) + exp.status.manualOffsetEv;
         console.log("EXPOSURE: exp.status.offsetEv =", exp.status.offsetEv);
         console.log("EXPOSURE: lastPhotoLum - exp.status.offsetEv =", lastPhotoLum - exp.status.offsetEv);
         local.first = false;
@@ -289,7 +289,7 @@ function calculateDelta(currentEv, lastPhotoLum, config) {
     exp.status.evMean = filteredMean(local.lumArray, trim);
     exp.status.evSlope = filteredSlope(local.evArray, trim) * config.targetTimeSeconds;
 
-    exp.status.offsetEv = getEvOffsetScale(currentEv, lastPhotoLum) - exp.status.manualOffsetEv;
+    exp.status.offsetEv = getEvOffsetScale(currentEv, lastPhotoLum) + exp.status.manualOffsetEv;
     return lastPhotoLum - exp.status.offsetEv;
 }
 
