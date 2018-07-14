7a956dcfb4c915aebd0e71294c7ecea2538db370
Elijah Parker <elijahparker@gmail.com>
2018-07-13 20:29:21 -0500

    bugfixes

--- intervalometer/exposure.js
+++ intervalometer/exposure.js
@@ -268,13 +268,13 @@ function calculateDelta(currentEv, lastPhotoLum, config) {
         exp.status.nightRefEv = lastPhotoLum * nightRatio + -1.5 * (1 - nightRatio);
         exp.status.dayRefEv = lastPhotoLum * (1 - nightRatio);
         exp.status.fixedRefEv = lastPhotoLum;
-        exp.status.manualOffsetEv = lastPhotoLum - getEvOffsetScale(currentEv, lastPhotoLum);
+        exp.status.manualOffsetEv = getEvOffsetScale(currentEv, lastPhotoLum);
         console.log("EXPOSURE: lastPhotoLum =", lastPhotoLum);
         console.log("EXPOSURE: exp.status.manualOffsetEv =", exp.status.manualOffsetEv);
         console.log("EXPOSURE: getEvOffsetScale(currentEv, lastPhotoLum) =", getEvOffsetScale(currentEv, lastPhotoLum));
         exp.status.offsetEv = getEvOffsetScale(currentEv, lastPhotoLum) + exp.status.manualOffsetEv;
         console.log("EXPOSURE: exp.status.offsetEv =", exp.status.offsetEv);
-        console.log("EXPOSURE: lastPhotoLum + exp.status.offsetEv =", lastPhotoLum - exp.status.offsetEv);
+        console.log("EXPOSURE: lastPhotoLum - exp.status.offsetEv =", lastPhotoLum - exp.status.offsetEv);
         local.first = false;
     }
 
@@ -308,10 +308,10 @@ function getEvOffsetScale(ev, lastPhotoLum, noAuto) {
         } else { // auto calculate night exposure
             evScale = [{
                 ev: exp.config.nightCompensationNightEv,
-                offset: -((lastPhotoLum - exp.status.nightRefEv) - -1.5) // -1.5
+                offset: -(exp.status.nightRefEv - -1.5) // -1.5
             }, {
                 ev: exp.config.nightCompensationDayEv,
-                offset: -((lastPhotoLum - exp.status.dayRefEv) - 0.0)
+                offset: -(exp.status.dayRefEv - 0.0)
             }]
         }
     } else {
