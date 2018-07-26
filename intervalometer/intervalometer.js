dfbcd7f96728a12200d20973fd784b4d2e284dd3
Elijah Parker <elijahparker@gmail.com>
2018-07-26 08:03:19 -0500

    allow constant motion w/o GPS

--- intervalometer/intervalometer.js
+++ intervalometer/intervalometer.js
@@ -375,7 +375,7 @@ function processKeyframes(setupFirst, callback) {
             if(axis.type == 'tracking' && !intervalometer.gpsData) {
                 axis.type = 'disabled';
                 intervalometer.emit('error', "No GPS/coordinates available for tracking calculations.  Time-lapse will continue with tracking disabled on axis " + m + ".");
-                checkDone('tracking');
+                return checkDone('tracking');
             }
             if(axis.type == 'tracking' && intervalometer.currentProgram.trackingTarget == 'sun' && sunPos) {
                 trackingTarget = calculateCelestialDistance(intervalometer.status.sunPos, sunPos, axis.trackBelowHorizon);
