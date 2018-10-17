f015be678a6d4f25906c20bd80828603abd24a74
Elijah Parker <elijahparker@gmail.com>
2018-10-17 17:03:38 -0500

    adding EOS R

--- lib/libgphoto2_ptp2_updates/library.c
+++ lib/libgphoto2_ptp2_updates/library.c
@@ -1947,6 +1947,9 @@ static struct {
 	/* https://github.com/gphoto/libgphoto2/issues/92 */
 	{"Canon:EOS 5D Mark IV",		0x04a9, 0x3281, PTP_CAP|PTP_CAP_PREVIEW|PTP_DONT_CLOSE_SESSION},
 
+	/* Elijah Parker <mail@timelapseplus.com> */
+	{"Canon:EOS 5D Mark IV",		0x04a9, 0x32da, PTP_CAP|PTP_CAP_PREVIEW|PTP_DONT_CLOSE_SESSION},
+
 	/* https://sourceforge.net/p/gphoto/feature-requests/445/ */
 	{"Canon:PowerShot Elph135",		0x04a9, 0x3288, PTPBUG_DELETE_SENDS_EVENT},
 
