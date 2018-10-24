2e19d31ac748067c115b539074328d3789f269e9
Elijah Parker <elijahparker@gmail.com>
2018-10-24 16:15:40 -0500

    sony aperture testing

--- camera/ptp/worker.js
+++ camera/ptp/worker.js
@@ -792,6 +792,7 @@ function set(item, value, callback) { // item can be 'iso', 'aperture', 'shutter
             for (var i = 0; i < list.length; i++) {
                 if (list[i].cameraName == value || list[i].name == value) {
                     console.log("WORKER:  (set " + item + " = " + (list[i].cameraName || value) + ")");
+                    if(item == 'f-number' && camera.model.match(/sony/i) && port != "SonyWifi") value = parseFloat(value);
                     camera.setConfigValue(item, list[i].cameraName || value, function(err) {
                         if(err) console.log("WORKER: (2) error setting " + item + " to '" + (list[i].cameraName || value) + "': ", err);
                         if (err) sendEvent('error', err);
