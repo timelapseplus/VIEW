0bc5e4ae9b08523d3db569430036a798f65ffa15
Elijah Parker <elijahparker@gmail.com>
2018-10-25 13:52:16 -0500

    a7iii testing

--- camera/ptp/worker.js
+++ camera/ptp/worker.js
@@ -800,7 +800,7 @@ function set(item, value, callback) { // item can be 'iso', 'aperture', 'shutter
                 if (list[i].cameraName == value || list[i].name == value) {
                     if(item == 'f-number' && camera.model.match(/sony/i) && port != "SonyWifi") value = parseFloat(value);
                     console.log("WORKER:  (set " + item + " = " + value + ") (" + typeof(value) + ")");
-                    camera.setConfigValue(item, list[i].cameraName || value, function(err) {
+                    camera.setConfigValue(item, value, function(err) {
                         if(err) console.log("WORKER: (2) error setting " + item + " to '" + value + "': ", err);
                         if (err) sendEvent('error', err);
                         if (callback) callback(err);
