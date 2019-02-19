e1b46b445ef6c5f9cb8569daf1f84c9589637d89
Elijah Parker <elijahparker@gmail.com>
2019-02-19 16:52:07 -0600

    st4 multi-axis move sync

--- motion/drivers/emotimo_st4.js
+++ motion/drivers/emotimo_st4.js
@@ -219,9 +219,22 @@ st4.setPosition = function(motorId, position, callback) {
 }
 
 st4.move = function(motorId, steps, callback) {
+	if(st4.status.moveStarted || st4.status.moving) {
+		if(!st4.connected) return callback && callback("not connected");
+		return setTimeout(function(){
+			st4.move(motorId, steps, callback);
+		}, 100);
+	}
+
 	if(!st4.movePreGroup) st4.movePreGroup = {};
-	if(!st4.movePreGroup[motorId]) st4.movePreGroup[motorId] = 0;
-	st4.movePreGroup[motorId] += steps;
+	if(!st4.movePreGroup[motorId]) {
+		st4.movePreGroup[motorId] = {
+			steps: 0,
+			callbacks: []
+		};
+	}
+	st4.movePreGroup[motorId].steps += steps;
+	st4.movePreGroup[motorId].callbacks.push(callback);
 
 	if(st4.movePreGroupHandle) {
 		return;
@@ -229,25 +242,26 @@ st4.move = function(motorId, steps, callback) {
 
 	st4.movePreGroupHandle = setTimeout(function(){
 		st4.movePreGroupHandle = null;
-
-		var args = {};
-		for(var mId in st4.movePreGroup) {
-			args[_motorName(mId)] = parseInt(st4.movePreGroup[mId] * _conversionFactor(mId) * _motorDirection(mId));
-		}
+		var group = st4.movePreGroup;
 		st4.movePreGroup = {};
 
-		if(st4.status.moveStarted || st4.status.moving) {
-			if(!st4.connected) return callback && callback("not connected");
-			return setTimeout(function(){
-				st4.move(motorId, steps, callback);
-			}, 100);
+		var args = {};
+		for(var mId in group) {
+			args[_motorName(mId)] = parseInt(group[mId].steps * _conversionFactor(mId) * _motorDirection(mId));
 		}
+
 		st4.status.moveStarted = true;
 		st4.status.moving = true;
 		_transaction('G2', args, function(err) {
 			st4.status.moveStarted = false;
 			if(err) return callback && callback(err);
-			_waitRunning(motorId, callback);
+			for(var mId in group) {
+				_waitRunning(mId, function(err, pos) {
+					for(var i = 0; i < group[mId].callbacks.length; i++) {
+						group[mId].callbacks[i] && group[mId].callbacks[i](err, pos);
+					}					
+				});
+			}
 		});
 	}, 100);
 }
