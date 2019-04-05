9a5c61cd6c9ecea5c68f44b51730c55e7261dd85
Elijah Parker <elijahparker@gmail.com>
2019-04-05 12:17:17 -0500

    wifi bugfixes and ronin testing

--- motion/drivers/ronin_s-test.js
+++ motion/drivers/ronin_s-test.js
@@ -3,10 +3,19 @@ var noble = require('noble');
 
 //var ronin = new Ronin();
 
-noble.on('discover', btDiscover);
-noble.startScanning([], false, function(err){
-    console.log("BLE scan started: ", err);
-});
+noble.on('stateChange', btStateChange);
+
+function btStateChange(state) {
+    console.log("CORE: BLE state changed to", state);
+    if (state == "poweredOn") {
+        setTimeout(function() {
+			noble.on('discover', btDiscover);
+			noble.startScanning([], false, function(err){
+			    console.log("BLE scan started: ", err);
+			});
+        });
+    }
+}
 
 function btDiscover(peripheral) {
 	console.log("BT peripheral", peripheral);
