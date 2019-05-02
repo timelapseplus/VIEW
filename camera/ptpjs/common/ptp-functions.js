4e6b549a8adfa870ba52bb4b73c0bb672dc5f362
Elijah Parker <elijahparker@gmail.com>
2019-05-02 12:26:55 -0500

    usb testing

--- camera/ptpjs/common/ptp-functions.js
+++ camera/ptpjs/common/ptp-functions.js
@@ -143,7 +143,7 @@ exports.transaction = function(cam, opcode, params, data, callback) {
 			if(!err && data) {
 				rlen = data.readUInt32LE(0);
 				rtype = data.readUInt16LE(4);
-				console.log("received packet type #", rtype, "size:", rlen);
+				console.log("received packet type #", rtype, "size:", rlen, "data length received:", data.length);
 				if(rtype == 3) {
 					console.log("completed transaction");
 					cb && cb(err, parseResponse(data), rbuf);
