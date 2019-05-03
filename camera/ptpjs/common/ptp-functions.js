cc0e29e919d2d3ca6ee2cef13860dd028fafacbd
Elijah Parker <elijahparker@gmail.com>
2019-05-03 10:43:23 -0500

    usb testing

--- camera/ptpjs/common/ptp-functions.js
+++ camera/ptpjs/common/ptp-functions.js
@@ -175,6 +175,29 @@ exports.extractJpeg = function(data) {
     	}
     }
 
+    var off = 0;
+    while(off < maxSearch) {
+        while(data[off] == 0xff) off++;
+        var mrkr = data[off];  off++;
+
+        if(mrkr == 0xd8) continue;    // SOI
+        if(mrkr == 0xd9) break;       // EOI
+        if(0xd0 <= mrkr && mrkr <= 0xd7) continue;
+        if(mrkr == 0x01) continue;    // TEM
+
+        var len = (data[off]<<8) | data[off+1];  off+=2;  
+
+        if(mrkr == 0xc0) {
+        	console.log({
+	            bpc : data[off],     // precission (bits per channel)
+	            w   : (data[off+1]<<8) | data[off+2],
+	            h   : (data[off+3]<<8) | data[off+4],
+	            cps : data[off+5]    // number of color components
+	        });
+	    }
+        off += len - 2;
+    }
+
     var jpegBuf = new Buffer(jpegEnd - jpegStart);
     data.copy(jpegBuf, 0, jpegStart, jpegEnd);
 
