c8c89609a288405cb9eccd2b54fd81b9272e266d
Elijah Parker <elijahparker@gmail.com>
2019-05-06 22:54:21 -0500

    usb testing

--- camera/ptpjs/drivers/fuji.js
+++ camera/ptpjs/drivers/fuji.js
@@ -392,7 +392,7 @@ driver.set = function(camera, param, value, callback) {
                     }
                 }
                 if(cameraValue !== null) {
-                    _//logD("setting", ptp.hex(properties[param].code), "to", cameraValue);
+                    logD("setting", ptp.hex(properties[param].code), "to", cameraValue);
                     properties[param].setFunction(camera._dev, properties[param].code, cameraValue, function(err) {
                         if(!err) {
                             camera[properties[param].category][param].current = mapPropertyItem(cameraValue, properties[param].values);
