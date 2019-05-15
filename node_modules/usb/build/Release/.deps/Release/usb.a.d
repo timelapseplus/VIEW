7b20ec4b83a82bf3b18d6c8e313ea11cc2605ebb
Elijah Parker <elijahparker@gmail.com>
2019-05-15 13:17:36 +0000

    usb module

---
+++ node_modules/usb/build/Release/.deps/Release/usb.a.d
@@ -0,0 +1,1 @@
+cmd_Release/usb.a := rm -rf "Release/usb.a" && cp -af "Release/obj.target/usb.a" "Release/usb.a"
---
+++ node_modules/usb/build/Release/.deps/Release/usb_bindings.node.d
@@ -0,0 +1,1 @@
+cmd_Release/usb_bindings.node := rm -rf "Release/usb_bindings.node" && cp -af "Release/obj.target/usb_bindings.node" "Release/usb_bindings.node"
