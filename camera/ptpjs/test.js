41ccd9287e7fa59d5453e639142faa1201592a85
Elijah Parker <elijahparker@gmail.com>
2019-06-14 16:50:43 -0500

    adding Nikon to new driver

--- camera/ptpjs/test.js
+++ camera/ptpjs/test.js
@@ -12,12 +12,16 @@ var requiredMethods = [
 ]
 
 var requiredObjects = [
-	'',
+	'supportedCameras',
 ]
 
 
 exports.driver = function(driver) {
 	for(var i = 0; i < requiredMethods.length; i++) {
 		if(typeof driver[requiredMethods[i]] != 'function') console.log("DRIVER TEST: missing " + requiredMethods[i] + " method");
+	}
+
+	for(var i = 0; i < requiredMethods.length; i++) {
+		if(typeof driver[requiredObjects[i]] != 'object') console.log("DRIVER TEST: missing " + requiredObjects[i] + " object");
 	}
 }
