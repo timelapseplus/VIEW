25e9bec7d9c16eea4d17322f2175aea185577ba4
Elijah Parker <elijahparker@gmail.com>
2019-07-09 11:27:55 -0500

    startup improvements

--- start.sh
+++ start.sh
@@ -58,7 +58,7 @@ cat ./logs/current.txt > ./logs/previous.txt
 echo $CORELOGFILE > ./logs/current.txt
 prepend_date() { while read line; do echo $(date +%Y%m%d-%H%M%S) $line; done }
 forever start -c "node --max_old_space_size=128" main.js 2>&1 | prepend_date >> $UILOGFILE &
-sleep(20)
+sleep 20
 test -e /lib/arm-linux-gnueabihf/libusb-0.1.so.4 && mv /lib/arm-linux-gnueabihf/libusb-0.1.so.4 /lib/arm-linux-gnueabihf/libusb--disabled--0.1.so.4 # disable libusb0.1 for Olympus support
 forever start -c "node --max_old_space_size=320" intervalometer/intervalometer-server.js 2>&1 | prepend_date >> $CORELOGFILE &
 
