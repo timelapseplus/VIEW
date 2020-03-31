5fc97cc955c28a541f6610f700d562f660ee2e92
Elijah Parker <elijahparker@gmail.com>
2020-03-31 14:06:44 -0500

    miops backlash correction testing

--- motion/drivers/miops.js
+++ motion/drivers/miops.js
@@ -408,7 +408,7 @@ MIOPS.prototype._backlashCorrection = function(targetPos) {
     }
 
     this._backlashOffset -= move * this._offsetDirection;
-    if(Math.abs(this._backlashOffset) > this._backlash) this._backlashOffset = this._backlash * -this._offsetDirection;
+    if(Math.abs(this._backlashOffset) > this._backlash && (this._backlashOffset / this._backlashOffset) == this._offsetDirection) this._backlashOffset = this._backlash * -this._offsetDirection;
     if(-this._backlashOffset * this._offsetDirection < 0) this._backlashOffset = 0;
 
     console.log("MIOPS(" + this._id + "): using backlash offset of", this._backlashOffset, "steps (moved", move, "steps)");
