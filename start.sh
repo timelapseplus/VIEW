050a64ff741fbf56703d881a25447cf66c455a5c
Elijah Parker <elijahparker@gmail.com>
2021-06-28 16:51:26 -0500

    schedule resume fixes

--- start.sh
+++ start.sh
@@ -10,9 +10,10 @@ if [ "$MD51" = "72cc8b90d9ecaff265e29a3b5c836b40  /root/startup.sh" ] || [ "$MD5
 then
 		test -e /home/view/current/media/view-splash-updating.png && cp /home/view/current/media/view-splash-updating.png /root/view-splash-updating.png
 		test -e /home/view/current/sd_card_update.js && cp /home/view/current/sd_card_update.js /root/sd_card_update.js
-		test -e /home/view/current/start.sh && cp /home/view/current/start.sh /root/startup.sh
         echo "md5 matches, copied files"
 fi
+
+test -e /home/view/current/start.sh && cp /home/view/current/start.sh /root/startup.sh
 
 if (/opt/sunxi-tools/pio -m PB10 | grep -q "PB10<0><1><0><0>") && test -e /sys/class/block/mmcblk1p1; then
     echo "button pressed"
@@ -29,6 +30,8 @@ if [ "$ACTION" = "install" ]; then
 	killall fbi
 	fbi -T 1 -d /dev/fb0 -noverbose /root/view-splash-updating.png &
 	killall node
+	sleep 1
+	killall -s 9 node
 	cd /root;
 	node sd_card_update.js
 	echo "installation complete!"
@@ -55,6 +58,8 @@ echo "STARTING processes...";
 killall fbi
 fbi -T 1 -d /dev/fb0 -noverbose /root/view-splash.png &
 killall node
+sleep 1
+killall -s 9 node
 cd /home/view/current;
 DATE=`date +"%Y%m%d-%H%M%S"`
 UILOGFILE="/var/log/view-ui-$DATE.txt"
